be91de508e9cfec0599bf50fb6a85d65430253c6
tJain74 <89228273+tJain74@users.noreply.github.com>
2021-08-19 18:46:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+
+
+function setup(){
+  var b2 = createButton("click here to Swap");
+  }
+
+
+function draw()
+{
+  background("pink");
+}
+function swap()
+{
+  [a, b] = [b, a]
+}
